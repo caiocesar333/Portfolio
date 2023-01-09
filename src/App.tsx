6257ed88b69f5b8445870ae367b228d88fadc626
Caio <caiocesar333@outlook.com>
2023-01-09 17:41:47 -0300

    feat: initial ajusts with theming and font-family

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { Container } from './style/global';
+
+const theme ={
+  dark:{
+    primary: "#C778DD",
+    text: "#fff",
+    background:"#282C33"
+  },
+  light:{
+    primary: "#C778DD",
+    text: "#282C33",
+  }
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container theme={theme.dark.background} color={theme.dark.text}>
+        hello
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './style';
+
+const Navbar: React.FC = () => {
+  return <Container>
+    
+  </Container>
+}
+
+export default Navbar;
