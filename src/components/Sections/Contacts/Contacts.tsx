b4601a0c40a06fa8e366a277b10f66d444009ef3
Caio <caiocesar333@outlook.com>
2023-01-16 20:36:11 -0300

    feat: adding contact information | fix: temporarily removing contact form for deploy

--- src/components/Sections/Contacts/Contacts.tsx
+++ src/components/Sections/Contacts/Contacts.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ContactForm } from '../../ContactForm/ContactForm';
+// import { ContactForm } from '../../ContactForm/ContactForm';
 import { LargeText } from '../../TextComponents/LargeText';
 import { SmallText } from '../../TextComponents/SmallText';
 
@@ -18,8 +18,13 @@ export function Contacts() {
                     <SmallText about="false" text="I’m interested in job opportunities. However, 
                     if you have a freelance request, don’t hesitate to contact me"/>
                 </TextWrapper>
+            <TextWrapper>
+                <SmallText text='Email: caiocesar333@outlook.com' color='true' about='false' />
+                <SmallText text='Whatsapp: +55 (71)993707525' color='true' about='false' />
+                <SmallText text='Linkedin: https://www.linkedin.com/in/caio-cesar-de-jesus-conceicao/' color='true' about='false' />
+            </TextWrapper>
             </Tab>
-            <ContactForm />
+            {/* <ContactForm /> NOT WORKING AT THE MOMENT */}
         </Container>
     )
 }
